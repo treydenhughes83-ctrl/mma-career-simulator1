2472def2aae9bdad585340c409e99f40b1b7b000
treydenhughes83-ctrl <treydenhughes83@gmail.com>
2026-07-18 12:54:54 +0100

    Create divisions.ts

---
+++ src/data/divisions.ts
@@ -0,0 +1,12 @@
+export const divisions = [
+  "Flyweight",
+  "Bantamweight",
+  "Featherweight",
+  "Lightweight",
+  "Welterweight",
+  "Middleweight",
+  "Light Heavyweight",
+  "Heavyweight",
+] as const;
+
+export type Division = typeof divisions[number];
